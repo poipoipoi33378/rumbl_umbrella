c6c4b54053fc088809ac92a2f529a3a3222e635d
seiichi <poipoipoi33378@gmail.com>
2020-09-12 17:40:21 +0900

    Chapter11

---
+++ apps/rumbl/test/rumbl/multimedia_test.exs
@@ -0,0 +1,84 @@
+defmodule Rumbl.MultimediaTest do
+  use Rumbl.DataCase, async: true
+
+  alias Rumbl.Multimedia
+  alias Rumbl.Multimedia.Category
+
+  describe "category" do
+    test "list_alpabetical_categories/0" do
+      for name <- ~w(Drama Action Comedy) do
+        Multimedia.create_category!(name)
+      end
+
+      alpha_names =
+        for %Category{name: name} <-
+              Multimedia.list_alpabetical_categories() do
+          name
+        end
+
+      assert alpha_names == ~w(Action Comedy Drama)
+    end
+  end
+
+  describe "video" do
+    alias Rumbl.Multimedia.Video
+
+    @valid_attrs %{description: "desc", title: "title", url: "http://local"}
+    @invalid_attrs %{description: nil, title: nil, url: nil}
+
+    setup do
+      {:ok, owner: user_fixture()}
+    end
+
+    test "list_videos/0 returns all videos", %{owner: owner} do
+      %Video{id: id1} = video_fixture(owner)
+      assert [%Video{id: ^id1}] = Multimedia.list_videos()
+      %Video{id: id2} = video_fixture(owner)
+      assert [%Video{id: ^id1}, %Video{id: ^id2}] = Multimedia.list_videos()
+    end
+
+    test "get_video!/1 returns the video with given id", %{owner: owner} do
+      %Video{id: id} = video_fixture(owner)
+      assert %Video{id: ^id} = Multimedia.get_video!(id)
+    end
+
+    test "create_video/2 with valid data creates a video", %{owner: owner} do
+      assert {:ok, %Video{} = video} = Multimedia.create_video(owner, @valid_attrs)
+      assert video.description == "desc"
+      assert video.title == "title"
+      assert video.url == "http://local"
+    end
+
+    test "create_video/2 with invalid data returns error changeset", %{owner: owner} do
+      assert {:error, %Ecto.Changeset{}} = Multimedia.create_video(owner, @invalid_attrs)
+    end
+
+    test "update_video/2 with valid data updates the video", %{owner: owner} do
+      video = video_fixture(owner)
+      assert {:ok, video} = Multimedia.update_video(video, %{title: "updated title"})
+      assert %Video{} = video
+      assert video.title == "updated title"
+    end
+
+    test "update_video/2 with invalid data returns error changeset", %{owner: owner} do
+      video = video_fixture(owner)
+      %Video{id: id} = video = video_fixture(owner)
+
+      assert {:error, %Ecto.Changeset{}} = Multimedia.update_video(video, @invalid_attrs)
+      assert %Video{id: ^id} = Multimedia.get_video!(id)
+    end
+
+    test "delete_video/1 delete the video", %{owner: owner} do
+      video = video_fixture(owner)
+
+      assert {:ok, %Video{}} = Multimedia.delete_video(video)
+      assert Multimedia.list_videos() == []
+    end
+
+    test "change_video/1 returns a video changeset", %{owner: owner} do
+      video = video_fixture(owner)
+
+      assert %Ecto.Changeset{} = Multimedia.change_video(video)
+    end
+  end
+end
---
+++ source.exs
@@ -0,0 +1,8 @@
+cp -R rumbl/lib/rumbl rumbl_umbrella/apps/rumbl/lib​ ​  ​$ ​​cp​​ ​​rumbl/lib/rumbl.ex​​ ​​rumbl_umbrella/apps/rumbl/lib​ ​  ​$ ​​cp​​ ​​-R​​ ​​rumbl/test/rumbl​​ ​​rumbl_umbrella/apps/rumbl/test​ ​  ​$ ​​cp​​ ​​-R​​ ​​rumbl/priv/repo​​ ​​rumbl_umbrella/apps/rumbl/priv​ ​  ​$ ​​cp​​ ​​rumbl/test/support/data_case.ex​​ ​​rumbl_umbrella/apps/rumbl/test/support​ ​  ​$ ​​cp​​ ​​rumbl/test/support/test_helpers.ex​​ ​​\​ ​  ​     ​​rumbl_umbrella/apps/rumbl/test/support​
+
+McCord, Chris. Programming Phoenix 1.4 (pp.386-387). Pragmatic Bookshelf. Kindle 版.
+
+
+                                                                                  cp​​ ​​-R​​ ​​rumbl/assets/js​​ ​​rumbl_umbrella/apps/rumbl_web/assets/​ ​  ​$ ​​cp​​ ​​-R​​ ​​rumbl/assets/css​​ ​​rumbl_umbrella/apps/rumbl_web/assets/​
+
+                                                                                                                                                                                                                                   McCord, Chris. Programming Phoenix 1.4 (p.389). Pragmatic Bookshelf. Kindle 版.
