e4d882d4d178c184b28a7fa69583ffb09787bb5d
seiichi <poipoipoi33378@gmail.com>
2020-09-14 16:43:09 +0900

    modify info_sys.ex

--- apps/info_sys/lib/info_sys/cache.ex
+++ apps/info_sys/lib/info_sys/cache.ex
@@ -17,25 +17,9 @@ defmodule InfoSys.Cache do
     GenServer.start_link(__MODULE__, opts, name: opts[:name])
   end
 
-  @clear_interval :timer.seconds(60)
-
   def init(opts) do
-    state = %{
-      interval: opts[:clear_interval] || @clear_interval,
-      timer: nil,
-      table: new_table(opts[:name])
-    }
-
-    {:ok, schedule_clear(state)}
-  end
-
-  def handle_info(:clear, state) do
-    :ets.delete_all_objects(state.table)
-    {:noreply, schedule_clear(state)}
-  end
-
-  defp schedule_clear(state) do
-    %{state | timer: Process.send_after(self(), :clear, state.interval)}
+    new_table(opts[:name])
+    {:ok, %{}}
   end
 
   defp new_table(name) do
@@ -46,8 +30,7 @@ defmodule InfoSys.Cache do
       :named_table,
       :public,
       read_concurrency: true,
-      write_concurrency: true
-    ])
+      write_concurrency: true])
   end
 
   defp tab_name(name), do: :"#{name}_cache"
