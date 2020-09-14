0f615631c3cc57b536fd420732d59f399171d7e9
seiichi <poipoipoi33378@gmail.com>
2020-09-14 16:30:06 +0900

    modify info_sys.ex

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -6,16 +6,25 @@ defmodule InfoSys do
   end
 
   def compute(query, opts \\ []) do
+    timeout = opts[:timeout] || 10_000
     opts = Keyword.put_new(opts, :limit, 10)
-    backends = opts[:backend] || @backends
+    backends = opts[:backends] || @backends
 
     backends
     |> Enum.map(&async_query(&1, query, opts))
+    |> Task.yield_many(timeout)
+    |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end)
+    |> Enum.flat_map(fn
+      {:ok, results} -> results
+      _ -> []
+    end)
+    |> Enum.sort(&(&1.score >= &2.score))
+    |> Enum.take(opts[:limit])
   end
 
   defp async_query(backend, query, opts) do
-    Task.Supervisor.async_nolink(InfoSys.TaskSupervisor, backend, :compute, [query, opts],
-      shutdown: :brutal_kill
+    Task.Supervisor.async_nolink(InfoSys.TaskSupervisor,
+      backend, :compute, [query, opts], shutdown: :brutal_kill
     )
   end
 end
