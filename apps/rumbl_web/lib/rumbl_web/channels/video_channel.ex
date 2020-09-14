955ffc13c48771bbb11d404b9835c1bfcea15750
seiichi <poipoipoi33378@gmail.com>
2020-09-14 19:37:12 +0900

    add compute_additional_info

--- apps/rumbl_web/lib/rumbl_web/channels/video_channel.ex
+++ apps/rumbl_web/lib/rumbl_web/channels/video_channel.ex
@@ -1,5 +1,6 @@
 defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
+
   alias Rumbl.{Accounts, Multimedia}
   alias RumblWeb.AnnotationView
 
@@ -19,37 +20,17 @@ defmodule RumblWeb.VideoChannel do
 
   def handle_info(:after_join, socket) do
     push(socket, "presence_state", RumblWeb.Presence.list(socket))
-
     {:ok, _} =
       RumblWeb.Presence.track(
         socket,
         socket.assigns.user_id,
-        %{device: "browser"}
-      )
-
+        %{device: "browser"})
     {:noreply, socket}
   end
 
   def handle_in(event, params, socket) do
     user = Accounts.get_user!(socket.assigns.user_id)
     handle_in(event, params, user, socket)
-  end
-
-  def handle_in("new_annotation", params, user, socket) do
-    case Multimedia.annotate_video(user, socket.assigns.video_id, params) do
-      {:ok, annotation} ->
-        broadcast!(socket, "new_annotation", %{
-          id: annotation.id,
-          user: RumblWeb.UserView.render("user.json", %{user: user}),
-          body: annotation.body,
-          at: annotation.at
-        })
-
-        {:reply, :ok, socket}
-
-      {:error, changeset} ->
-        {:reply, {:error, %{error: changeset}}, socket}
-    end
   end
 
   def handle_in("new_annotation", params, user, socket) do
@@ -76,19 +57,16 @@ defmodule RumblWeb.VideoChannel do
   defp compute_additional_info(annotation, socket) do
     for result <-
           InfoSys.compute(annotation.body, limit: 1, timeout: 10_000) do
+
       backend_user = Accounts.get_user_by(username: result.backend.name())
       attrs = %{body: result.text, at: annotation.at}
 
       case Multimedia.annotate_video(
-             backend_user,
-             annotation.video_id,
-             attrs
-           ) do
+             backend_user, annotation.video_id, attrs) do
+
         {:ok, info_ann} ->
           broadcast_annotation(socket, backend_user, info_ann)
-
-        {:error, _changeset} ->
-          :ignore
+        {:error, _changeset} -> :ignore
       end
     end
   end
